d9e794be6806207475af8379bceb6382dc0840fc
zhanghong <2382725346@qq.com>
2019-07-10 18:28:43 +0800

    修改BUG

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -14,7 +14,11 @@ $(document).ready(function(){
 				for(var i=0,i<result.length,i++){
 				$(".arctiles_title").html(result[0].title)
 				$(".arctile_type").html(result[0].classify)
-				$(".arctile_author").html(result[0].text)
+				var author = result[0].author
+				if ($.isEmptyObject(author)){
+					author = '匿名'
+				}
+				$(".arctile_author").html(author)
 				$(".arctile_update").html(result[0].date)
 				}
 
